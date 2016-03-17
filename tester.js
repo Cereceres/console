615a57c627a65def7b1a88d1b3ae797db56632fa
cereceres <jesus.edelcereceres@gmail.com>
2016-03-17 17:46:38 -0600

    the % of frauds is added to log

--- tester.js
+++ tester.js
@@ -1,9 +1,11 @@
 'use strict'
-const faker = require( 'faker' )
-const Coevent = require( 'co-eventemitter' )
-const Psp = require( 'psp-sdk-js' )
-const config = require( './config' )
-const fs = require( 'fs' )
+const faker = require('faker')
+const Coevent = require('co-eventemitter')
+const Psp = require('psp-sdk-js')
+const config = require('./config')
+const fs = require('fs')
+let n = 0,
+  frauds = 0
   // Kamji connection
   // Kamji connection
   //
@@ -27,101 +29,105 @@ global.fixtures.store = {
 
 }
 
-let psp = new Psp( {
+let psp = new Psp({
   email: global.fixtures.store.email,
   password: global.fixtures.store.password,
   host: global.fixtures.connection.host,
-  key: fs.readFileSync( './keys/unauthorized-client-key.pem' ),
-  cert: fs.readFileSync( './keys/unauthorized-client-cert.pem' ),
-  ca: fs.readFileSync( './keys/server-cert.pem' )
-} )
-let coevent = new Coevent( ),
-  customer, Customers = [ ],
+  key: fs.readFileSync('./keys/unauthorized-client-key.pem'),
+  cert: fs.readFileSync('./keys/unauthorized-client-cert.pem'),
+  ca: fs.readFileSync('./keys/server-cert.pem')
+})
+let coevent = new Coevent(),
+  customer, Customers = [],
   card, Cards = {},
-  payment, Payments = [ ],
+  payment, Payments = [],
   i, j, ncards
 
-coevent.on( 'create', function* ( number ) {
-  yield psp.connect( )
-  console.log( 'creando:', number.customer );
-  for ( i = 0; i < number.customer; i++ ) {
+coevent.on('create', function*(number) {
+  yield psp.connect()
+  console.log('creando:', number.customer);
+  for (i = 0; i < number.customer; i++) {
     customer = {
-      firstname: faker.name.firstName( ),
-      lastname: faker.name.lastName( ),
-      email: faker.internet.email( )
+      firstname: faker.name.firstName(),
+      lastname: faker.name.lastName(),
+      email: faker.internet.email()
     }
-    Customers.push( customer )
+    Customers.push(customer)
   }
 
-  for ( j = 0; j < number.customer; j++ ) {
-    customer = Customers[ j ]
-    Cards[ customer ] = [ ]
-    ncards = Math.floor( Math.random( ) * number.card + 1 )
-    console.log( 'creando cards:', ncards );
-    for ( i = 0; i < ncards; i++ ) {
-      card = new psp.Card( {
+  for (j = 0; j < number.customer; j++) {
+    customer = Customers[j]
+    Cards[customer] = []
+    ncards = Math.floor(Math.random() * number.card + 1)
+    for (i = 0; i < ncards; i++) {
+      card = new psp.Card({
         firstname: customer.firstname,
         lastname: customer.lastname,
         card: '4242424242424242',
-        expiry_year: ( Math.floor( Math.random( ) * 30 ) + 2016 )
-          .toString( ),
+        expiry_year: (Math.floor(Math.random() * 30) + 2016)
+          .toString(),
         expiry_month: '08',
-        cvv: Math.floor( Math.random( ) * 10 )
-          .toString( ) + Math.floor( Math.random( ) * 10 )
-          .toString( ) + Math.floor( Math.random( ) * 10 )
-          .toString( )
-      } )
-      console.log( 'guardando card' );
-      card = yield card.save( {
+        cvv: Math.floor(Math.random() * 10)
+          .toString() + Math.floor(Math.random() * 10)
+          .toString() + Math.floor(Math.random() * 10)
+          .toString()
+      })
+      card = yield card.save({
         lean: true
-      } )
-      console.log( 'card saved=', card );
-      Cards[ customer ].push( card )
+      })
+      console.log('card saved=', card);
+      Cards[customer].push(card)
     }
   }
-} )
-coevent.on( 'pay', function* ( ) {
-  customer = Customers[ Math.floor( Math.random( ) * Customers.length ) ]
-  card = Cards[ customer ][ Math.floor( Math.random( ) * Cards[ customer ].length ) ]
-  payment = new psp.Payment( {
-    amount: Math.floor( Math.random( ) * 5000 ),
+})
+coevent.on('pay', function*() {
+  n++
+  customer = Customers[Math.floor(Math.random() * Customers.length)]
+  card = Cards[customer][Math.floor(Math.random() * Cards[customer].length)]
+  payment = new psp.Payment({
+    amount: Math.floor(Math.random() * 4997) + 3,
     token: card.token,
     firstname: customer.firstname,
     lastname: customer.lastname,
     email: customer.email,
-    items: [ {
+    items: [{
       item: 'totest'
-    } ],
-    referenceId: ( Math.floor( Math.random( ) * 100000000000000000000 ) )
-      .toString( )
-  } )
-  payment = yield payment.save( {
+    }],
+    referenceId: (Math.floor(Math.random() * 100000000000000000000))
+      .toString()
+  })
+  payment = yield payment.save({
     lean: true
-  } )
+  })
+  if (payment.isFraud) {
+    frauds++
+  }
   console.log(
     'fraudProbability =' + payment.fraudProbability +
     '\n',
-    'amount:' + parseFloat( payment.amount ) + '\n',
+    'amount:' + parseFloat(payment.amount) + '\n',
     'isFraud =' + payment.isFraud + '\n',
-    'status =' + payment.status + '\n' )
-  Payments.push( payment )
+    'status =' + payment.status + '\n')
+  console.log('=====================\n');
+  console.log('% Frauds = ' + Math.floor(frauds / n * 100) + '\n');
+  console.log('=====================\n');
 
-} )
-console.log( 'antes de create' );
-coevent.emit( 'create', {
-    customer: 5,
-    card: 4
-  } )
-  .then( function ( ) {
-    console.log( 'all faker data are created' );
-    setInterval( function ( ) {
-      coevent.emit( 'pay' )
-    }, 2500 )
-  } )
-  .catch( function ( e ) {
-    console.log( 'errror happen', e );
-  } )
+  Payments.push(payment)
+})
+coevent.emit('create', {
+    customer: 10000,
+    card: 20
+  })
+  .then(function() {
+    console.log('all faker data are created');
+    setInterval(function() {
+      coevent.emit('pay')
+    }, 100)
+  })
+  .catch(function(e) {
+    console.log('errror happen', e);
+  })
 
-process.on( 'uncaughtException', ( err ) => {
-  console.log( `Caught exception: ${err}` );
-} );
+process.on('uncaughtException', (err) => {
+  console.log(`Caught exception: ${err}`);
+});
